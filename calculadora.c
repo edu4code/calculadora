22fba13eee1a7c9b67028dfa451c1183ab1249b1
edu4code <du.araujos28@gmail.com>
2026-06-02 19:06:26 -0300

    adicionando função de subtração

--- calculadora.c
+++ calculadora.c
@@ -5,8 +5,14 @@ int soma(int a, int b)
     return a + b;
 }
 
+int subtracao(int a, int b)
+{
+    return a - b;
+}
+
 int main()
 {
     printf("Soma: %d\n", soma(3, 4));
+    printf("Subtracao: %d\n", subtracao(10, 3));
     return 0;
 }
